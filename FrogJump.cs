5bef3862d956a7b38011f919739cb4744ef6c26b
GowthamF <40605124+GowthamF@users.noreply.github.com>
2019-03-25 20:01:11 +0530

    Add files via upload

---
+++ FrogJump.cs
@@ -0,0 +1,77 @@
+using System;
+
+
+namespace FrogJumps
+{
+	public class FrogJump
+	{
+		static void Main(string [] args)
+		{
+			int metersToJump=Int32.Parse(Console.ReadLine());
+			Console.WriteLine(CalculateJumps(metersToJump));
+		
+		}
+
+		public static string CalculateJumps(int metersToJump)
+		{
+			Frog frog=new Frog();
+			int hopsCounts=0;
+			int restTimes=0;
+			
+			while(metersToJump>0)
+			{
+				if(metersToJump<5)
+				{
+					hopsCounts++;
+					metersToJump-=metersToJump;	
+				}
+
+				if(metersToJump>=5)
+				{
+					hopsCounts++;
+					restTimes+=2;
+					metersToJump-=5;
+					
+					if(metersToJump>=3)
+					{
+						hopsCounts++;
+						restTimes+=3;
+						metersToJump-=3;	
+					}
+					
+					if(metersToJump>=2)
+					{
+						hopsCounts++;
+						restTimes+=5;
+						metersToJump-=2;
+					}
+							
+				}
+			}
+			
+			frog.HopsCount=hopsCounts;
+			frog.RestTime=restTimes;
+			
+				
+			
+			return frog.ToString();
+			
+		}
+
+	
+	}
+
+
+	class Frog
+	{
+		public int HopsCount{get;set;}
+	
+		public int RestTime{get;set;}
+
+	
+		public override string ToString()
+		{
+			return $"{HopsCount} Hops and {RestTime}s Rest Time";
+		}
+	}
+}
